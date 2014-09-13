2eec140456b3f7dd9d4163f421bf779f57ce7ac6
Mat Wszedybyl <mwszedybyl@Admins-MacBook-Pro-3.local>
2014-09-13 16:13:38 -0400

    first commit

---
+++ app/src/main/java/com/mkph/mateusz/dotagameapp/Fragments/WitchDoctorFragment.java
@@ -0,0 +1,30 @@
+package com.mkph.mateusz.dotagameapp.Fragments;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mkph.mateusz.dotagameapp.R;
+
+public class WitchDoctorFragment extends Fragment {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        mediaPlayer = MediaPlayer.create(getActivity(), R.raw.wdoc_win_03);
+       // play();
+        return inflater.inflate(R.layout.witch_doctor_fragment_layout, container, false);
+    }
+
+    public void play(){
+        mediaPlayer.start();
+    }
+
+
+}
